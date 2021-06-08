29c8e10b53fc999d041c721e56dea2638d5d6c0e
rubenbarbieri <rubenbarbieri11@gmail.com>
2021-06-08 15:06:23 +0200

    Aggiunta Main Empty Activity

---
+++ app/src/main/java/com/pideruben/guineaproject/MainAct.java
@@ -0,0 +1,14 @@
+package com.pideruben.guineaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
