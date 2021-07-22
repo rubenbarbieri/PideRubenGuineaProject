ad6531ede06030a8d1119a4be31ed7d9e530e654
Lorenzo Iacopetta <lorenzo.iaco@hotmail.com>
2021-07-22 22:37:52 +0200

    cannot use database on main thread perche potrebbe bloccare il processo per molto tempo... forse l'uso di fragment risolve il problema

--- app/src/main/java/com/pideruben/guineaproject/application/MainActivity.java
+++ app/src/main/java/com/pideruben/guineaproject/application/MainActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import com.pideruben.guineaproject.R;
+import com.pideruben.guineaproject.domain.Dipendente;
 import com.pideruben.guineaproject.persistence.AppDatabase;
+import com.pideruben.guineaproject.persistence.DaoDipendenti;
+import com.pideruben.guineaproject.persistence.EntityDipendente;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button addButton = findViewById(R.id.buttonAdd);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
+                DaoDipendenti daoDipendenti = db.daoDipendenti();
+                daoDipendenti.inserisciDipendente(new EntityDipendente("Lorenzo", "Iacopetta"));
+                System.out.println("Aggiunto dipendente");
+            }
+        });
+
+        final Button showButton = findViewById(R.id.buttonShow);
+        showButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
+                DaoDipendenti daoDipendenti = db.daoDipendenti();
+                ArrayList<EntityDipendente> dipendenti = (ArrayList<EntityDipendente>) daoDipendenti.getDipendenti();
+                for(EntityDipendente e : dipendenti){
+                    System.out.println(e);
+                }
+            }
+        });
     }
 }
