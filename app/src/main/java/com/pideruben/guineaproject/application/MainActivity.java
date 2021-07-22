9bf60689e9cf27df0caed471b057c066898ba856
Lorenzo Iacopetta <lorenzo.iaco@hotmail.com>
2021-07-22 13:32:48 +0200

    aggiunta main activity

--- app/src/main/java/com/pideruben/guineaproject/application/MainActivity.java
+++ app/src/main/java/com/pideruben/guineaproject/application/MainActivity.java
@@ -1,14 +1,15 @@
-package com.pideruben.guineaproject;
+package com.pideruben.guineaproject.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.pideruben.guineaproject.R;
 
 import android.os.Bundle;
 
-public class MainAct extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main);
     }
 }
